e1a17c6d8851881ab5110c932f14f9c8533f8973
aortegaramirez <aor.oaz@gmail.com>
2019-07-25 11:50:48 +0100

    Actualizacion 20190725. Se añade imagen meteorito por XML y por codigo, aunque este ultimo se encuentra comentado.

--- app/src/main/java/com/example/a1_juegorocas/MainActivity.java
+++ app/src/main/java/com/example/a1_juegorocas/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a1_juegorocas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creacion de variable para asignar la imagen.
+
+        /*ImageView imagenRoca = (ImageView)findViewById(R.id.imagenRoca);
+        imagenRoca.setImageResource(R.drawable.meteorito);*/
+
     }
 }
